6aa7dcbe53987d115f376269089a499651e0e7a0
guanchaoguo <771668687@qq.com>
2023-08-10 17:54:49 +0800

    ID DB

--- WebApplication1/Controllers/InformationController.cs
+++ WebApplication1/Controllers/InformationController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 using WebApplication1.Data;
 using WebApplication1.Models;
 
@@ -8,20 +9,26 @@ namespace WebApplication1.Controllers;
 [Route("[controller]")]
 public class InformationController
 {
-	InformationRecords informationRecords = new InformationRecords();
+    private InformationRecords informationRecords;
 
-	[HttpGet]
-	public IEnumerable<Information> Get()
-	{
-		List<Information> information = informationRecords.GetInformationRecords().Result;
+    public InformationController(MySqlConnection connection)
+    {
+        informationRecords = new InformationRecords(connection);
+    }
 
-		return Enumerable
-			.Range(0, information.Count)
-			.Select(index => new Information
-			{
-				Name = information[index].Name,
-				Title = information[index].Title,
-				Summary = information[index].Summary
-			});
-	}
+
+    [HttpGet]
+    public IEnumerable<Information> Get()
+    {
+        List<Information> information = informationRecords.GetInformationRecords().Result;
+
+        return Enumerable
+            .Range(0, information.Count)
+            .Select(index => new Information
+            {
+                Name = information[index].Name,
+                Title = information[index].Title,
+                Summary = information[index].Summary
+            });
+    }
 }
--- WebApplication1/Data/InformationRecords.cs
+++ WebApplication1/Data/InformationRecords.cs
@@ -3,16 +3,23 @@ using MySqlConnector;
 
 namespace WebApplication1.Data
 {
-	public class InformationRecords
-	{
-		private static readonly List<Information> information = new List<Information>();
-		private Information stevenInfo = new Information();
-		private MySqlConnection connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=ken_tutorial");
-        
-		public async Task<List<Information>> GetInformationRecords()
-		{
-            await connection.OpenAsync();
-            var command = new MySqlCommand("SELECT * FROM information", connection);
+    public class InformationRecords
+    {
+        private static readonly List<Information> information = new List<Information>();
+        private Information stevenInfo = new Information();
+        // private MySqlConnection connection = new MySqlConnection("Server=localhost;User ID=root;Password=root;Database=ken_tutorial");
+
+        private readonly MySqlConnection _connection;
+
+        public InformationRecords(MySqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<List<Information>> GetInformationRecords()
+        {
+            await _connection.OpenAsync();
+            var command = new MySqlCommand("SELECT * FROM information", _connection);
             var reader = await command.ExecuteReaderAsync();
             var tableRead = await reader.ReadAsync();
 
@@ -26,6 +33,6 @@ namespace WebApplication1.Data
             information.Add(stevenInfo);
 
             return information;
-		}
-	}
+        }
+    }
 }
